80fc386c9ec25c8f106207a1c25de525130fef2e
Noah Kirwan <mrnoahkirwan@gmail.com>
2026-03-18 16:26:45 +0000

    Add files via upload

--- models/app-info.js
+++ models/app-info.js
@@ -1,14 +1,13 @@
 'use strict';
 
-import logger from "../utils/logger.js";
-import appInfoData from "./app-info.json" assert { type: "json" };
+import appInfoData from "./app-info.json" with { type: "json" };
 
 const getAppInfo = (allAlbums) => {
   const albums = allAlbums || [];
   return {
     ...appInfoData,
     Albums: albums.length,
-    tracks: albums.reduce((total, p) => total + p.tracks.length, 0)
+    tracks: albums.reduce((total, album) => total + album.tracks.length, 0)
   };
 };
 
